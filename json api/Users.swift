5bb7b1853b8e56bd973f402db135b43f6a07268f
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2021-09-02 12:27:52 +0300

    JSON api

---
+++ json api/Users.swift
@@ -0,0 +1,14 @@
+//
+//  Users.swift
+//  json api
+//
+//  Created by Abdirizak Hassan on 9/2/21.
+//
+
+import Foundation
+
+struct User: Codable {
+    var id: Int
+    var name: String
+    var email: String
+}
--- json api/ViewController.swift
+++ json api/ViewController.swift
@@ -7,13 +7,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var users = [User]()
+    var apiURL = "https://jsonplaceholder.typicode.com/users"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "fetch users APi"
+        getUser()
     }
-
+    
+    
+    func getUser() {
+        if let url = URL(string: apiURL) {
+            URLSession.shared.dataTask(with: url) { (data, response, error) in
+                DispatchQueue.main.async {
+                    if let res = response as? HTTPURLResponse {
+                        print(res.statusCode)
+                    }
+                    if let data = data {
+                        //print(String(data: data, encoding: .utf8) ?? "")
+                        self.parse(json: data)
+                    }
+                    
+                    if let err = error {
+                        print(err)
+                    }
+                }
+                
+            }.resume()
+        }
+    }
+    
+    
+    func parse(json: Data) {
+        do {
+            let result = try JSONDecoder().decode([User].self, from: json)
+            print("result: ", result)
+            self.users = result
+            tableView.reloadData()
+        } catch {
+            print(error)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let user = users[indexPath.row]
+        cell.textLabel?.text = String(user.id) + user.name
+        cell.detailTextLabel?.text = user.email
+        return cell
+    }
+        
 
 }
 
